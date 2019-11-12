ac1b11da07729e3ac1d746cba10b69f53a93f564
Chris Morgan <hello@cloud42.io>
2019-11-12 15:18:36 +0000

    Added bash function for creating tfstate s3 bucket

---
+++ linux/bash/functions/aws_create_s3bucket_for_tfstate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+bucket_name=$1
+aws_region=$2
+
+create_tfstate_s3 () {
+
+which aws
+
+if [ $? -ne 0 ]; then
+   printf "\n\nThe AWS CLI is not installed...exiting\n\n\n"
+   exit
+else
+   aws s3api create-bucket --bucket $bucket_name --region $aws_region --create-bucket-configuration LocationConstraint=$aws_region
+   aws s3api put-bucket-versioning --bucket $bucket_name --versioning-configuration Status=Enabled
+   aws s3api put-public-access-block --bucket $bucket_name --public-access-block-configuration IgnorePublicAcls=true
+fi
+}
+
+create_tfstate_s3
+
+# ---------------------------------
+#
+# execution example: ./s3create.sh bucketname us-east-2
+#
+# ---------------------------------
