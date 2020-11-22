7d5313b4201c9909dbe6fd3a0d2d46086969360e
Cloud42 <hello@cloud42.io>
2020-11-22 21:33:13 +0000

    Add ec2 find all tags, all regions

---
+++ python/ec2_find_all_tags_all_regions.py
@@ -0,0 +1,16 @@
+import boto3
+import json
+
+regions=['us-east-2','us-east-1','ap-south-1','eu-west-1','eu-west-2']
+
+def main():
+  for region in regions:
+    ec2client = boto3.client('ec2',region_name=region)
+    response  = ec2client.describe_instances()
+    for reservation in response["Reservations"]:
+      for instance in reservation["Instances"]:
+        print(instance["Tags"])
+
+
+if __name__=="__main__":
+  main()
