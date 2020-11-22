0f6c250c253dc713d3e88182345f25f4c64539b7
Cloud42 <hello@cloud42.io>
2020-11-22 22:24:54 +0000

    Improve output. Make keys into list

--- python/ec2_find_all_tags_all_regions.py
+++ python/ec2_find_all_tags_all_regions.py
@@ -5,14 +5,17 @@ regions=['us-east-2','us-east-1','ap-south-1','eu-west-1','eu-west-2']
 
 def main():
   for region in regions:
+    keys = []
     ec2client = boto3.client('ec2',region_name=region)
     response  = ec2client.describe_instances()
     for reservation in response["Reservations"]:
       for instance in reservation["Instances"]:
         tags = instance["Tags"]
         for item in tags:
-          print(item)
+          keys.append(item['Key'])
 
+  tagslist = list( dict.fromkeys(keys) )
+  print(f"Unique tag keys across all regions = {tagslist}")
 
 if __name__=="__main__":
   main()
