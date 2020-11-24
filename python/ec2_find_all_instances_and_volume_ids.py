7056539b3525e18b7d716056c958f865c86801d4
Cloud42 <hello@cloud42.io>
2020-11-24 16:54:46 +0000

    Improve output

--- python/ec2_find_all_instances_and_volume_ids.py
+++ python/ec2_find_all_instances_and_volume_ids.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import boto3
 
 regions=['us-east-2','us-east-1','ap-south-1','eu-west-2','eu-west-1']
 
 def main():
   full_instance_dict = instance_ids_with_volumes()
-  print(full_instance_dict)
+  for key,value in full_instance_dict.items():
+    print('Found instance ' + key + ' in region ' + value[0] + ' with attached volumes ' + str(value[1]))
 
 def instance_ids_with_volumes():
   instance_ids_with_volumes_dict = {}
@@ -17,8 +18,8 @@ def instance_ids_with_volumes():
     volume_id_list=[]
     for item in instance.volumes.all():
       volume_id_list.append(item.id)
-
-    instance_ids_with_volumes_dict[key] = volume_id_list 
+    instance_ids_with_volumes_dict.setdefault(key, []).append(value)
+    instance_ids_with_volumes_dict.setdefault(key, []).append(volume_id_list)
   return dict(instance_ids_with_volumes_dict)
 
 def instance_ids():
