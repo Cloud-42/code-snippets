7cb34d87f1df2a8e0723a71f27a1aa822ca6c1e1
Cloud-42 <hello@cloud42.io>
2021-05-26 15:17:53 +0000

    re-structure s3 scripts

---
+++ python/s3/s3_versioned_bucket_clean.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import boto3
+
+def s3_delete_all(**kwargs):
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(**kwargs)
+    bucket.object_versions.all().delete()
+
+name = input("Enter the bucket name: ")
+
+def main():
+   s3_delete_all(
+     name=name
+   )
+
+
+if __name__=="__main__":
+   main()
+
+# Enter the name using format "BUCKET_NAME"
