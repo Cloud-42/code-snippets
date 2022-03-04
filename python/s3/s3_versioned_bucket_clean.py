404f76c9806bfe57d6b0253c778ad6cea6a19fd1
Cloud-42 <hello@cloud42.io>
2022-03-04 16:36:40 +0000

    Improve output for s3 purge

--- python/s3/s3_versioned_bucket_clean.py
+++ python/s3/s3_versioned_bucket_clean.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 import boto3
+import sys
+import logging
+import pprint
 
 def s3_delete_all(**kwargs):
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(**kwargs)
-    bucket.object_versions.all().delete()
+    try: 
+      response = bucket.object_versions.all().delete()
+      pprint.pprint(response[0]['Deleted'])
+    except Exception as e:
+     print(str(e))
 
+ 
 name = input("Enter the bucket name: ")
 
 def main():
