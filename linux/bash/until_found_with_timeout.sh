b5b6e2ae06cd3d506da6b0035de2ea391248f287
Cloud-42 <hello@cloud42.io>
2022-11-03 23:38:50 +0000

    Update until_found_with_timeout.sh

--- linux/bash/until_found_with_timeout.sh
+++ linux/bash/until_found_with_timeout.sh
@@ -1,6 +1,6 @@
-WAIT=0
-until [ $WAIT -eq 20 ] || [ "$(kubectl get secrets -n dev | grep postgres)" ]; do
-  echo "Waiting for RDS secret to be present. Wait count = $WAIT"
+WAIT_COUNT=0
+until [ $WAIT_COUNT -eq 20 ] || [ "$(kubectl get secrets -n dev | grep postgres)" ]; do
+  echo "Waiting for RDS secret to be present. Wait count = $WAIT_COUNT"
   sleep 60
-  (( WAIT++ ))
+  WAIT_COUNT=$(($WAIT_COUNT+1)) 
 done
