5c8301d11cfeea0030dccee2de05fdd9ec68fcae
Cloud-42 <hello@cloud42.io>
2023-06-14 22:19:14 +0100

    feat: improve s3 purging script

--- python/s3/s3_versioned_bucket_clean.py
+++ python/s3/s3_versioned_bucket_clean.py
@@ -1,29 +1,45 @@
-#!/usr/bin/env python
-import boto3
-import sys
+#!/usr/bin/env python3
+
 import logging
-import pprint
-
-def s3_delete_all(**kwargs):
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(**kwargs)
-    try: 
-      response = bucket.object_versions.all().delete()
-      pprint.pprint(response[0]['Deleted'])
-    except Exception as e:
-     print(str(e))
-
- 
-name = input("Enter the bucket name: ")
-
-def main():
-   s3_delete_all(
-     name=name
-   )
+import boto3
+from botocore.exceptions import ClientError
+logger = logging.getLogger(__name__)
 
 
-if __name__=="__main__":
-   main()
+class Deletes3:
 
-# Enter the name using format "BUCKET_NAME"
-# Has to be a string in ""
+    def __init__(self, filename: str = "./bucketNames.txt") -> None:
+        self.filename: str = filename
+        self.bucket_names: list = self.get_bucket_names(self.filename)
+
+    def get_bucket_names(self, filename: str) -> list:
+        bucket_names: list = []
+        with open(filename) as f:
+            for name in f.readlines():
+                name = name.rstrip().strip('"')
+                bucket_names.append(name.split(":")[-1])
+
+        return bucket_names
+
+    def remove_buckets(self, bucket_names: list) -> None:
+        for bucket_name in bucket_names:
+
+            logger.info(f"purging versions from {bucket_name}")
+
+            try:
+                s3 = boto3.resource('s3')
+                bucket = s3.Bucket(bucket_name)
+
+                logger.info(f"purging versions from {bucket_name}")
+                bucket.object_versions.all().delete()
+
+                logger.info(f"deleting bucket: s3://{bucket_name}")
+                response = bucket.delete()
+
+            except ClientError as error:
+                logger.exception(f"Couldn't delete bucket {bucket}")
+
+
+if __name__ == "__main__":
+    c = Deletes3()
+    c.remove_buckets(c.bucket_names)
