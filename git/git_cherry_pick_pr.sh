3d3d1f7601d265feeba42b18c01a1c109df4e8df
Cloud-42 <hello@cloud42.io>
2023-11-07 16:45:33 +0000

    feat: git_cherry_pick_pr.sh

---
+++ git/git_cherry_pick_pr.sh
@@ -0,0 +1,8 @@
+git clone REPO
+git fetch --all
+git checkout BRANCH WITH COMMIT ON 
+git log ( take note of commit id )
+git checkout TARGET BRANCH 
+git cherry-pick COMMIT ID
+git checkout -b feat/new_branch
+git push origin feat/new_branch
